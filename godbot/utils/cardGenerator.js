de57e36ebe57e69f6de326e8bf7942e472229552
Mysian <lym3123@naver.com>
2025-06-08 21:28:01 +0900

    Delete godbot/utils directory

--- godbot/utils/cardGenerator.js
+++
@@ -1,9 +0,0 @@
-const { getAllCards } = require("../config/cardData");
-
-function generateRandomCard() {
-  const cards = getAllCards();
-  const randomIndex = Math.floor(Math.random() * cards.length);
-  return cards[randomIndex];
-}
-
-module.exports = generateRandomCard;
