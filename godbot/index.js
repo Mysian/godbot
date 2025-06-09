b2277238659123f57faa6fa57bff81e6a934b115
Mysian <lym3123@naver.com>
2025-06-09 09:45:17 +0900

    Update index.js

--- godbot/index.js
+++ godbot/index.js
@@ -11,6 +11,7 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildPresences,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
   ],
 });
 
