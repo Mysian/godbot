100050117aa546685d6c4822b99e042ae8a9e87f
Mysian <lym3123@naver.com>
2025-06-16 18:57:00 +0900

    Update passive.js

--- godbot/battle-system/passive.js
+++ godbot/battle-system/passive.js
@@ -1,11 +1,10 @@
 // battle-system/passive.js
-const passives = require('../utils/passive-skills'); // 패시브 데이터 파일
+const passives = require('../utils/passive-skills'); 
 module.exports = function resolvePassive(user, enemy, context, event) {
   let logs = [];
   const champName = user.name;
-  // 패시브는 각 챔피언마다 events 구조로 (onAttack, onDefend, ...) 구현!
-  if (passives[champName] && passives[champName].events && typeof passives[champName].events[event] === 'function') {
-    const log = passives[champName].events[event](user, enemy, context);
+  if (passives[champName] && typeof passives[champName].passive === 'function') {
+    const log = passives[champName].passive(user, enemy, context, event);
     if (log) logs.push(log);
   }
   return logs;
