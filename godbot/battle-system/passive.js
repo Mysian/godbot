f07a1605a4e90c6a72162c5897d1eb095b975c65
Mysian <lym3123@naver.com>
2025-06-17 00:11:23 +0900

    Update passive.js

--- godbot/battle-system/passive.js
+++ godbot/battle-system/passive.js
@@ -1,11 +1,14 @@
 // battle-system/passive.js
-const passives = require('../utils/passive-skills'); 
-module.exports = function resolvePassive(user, enemy, context, event) {
+const passives = require('../utils/passive-skills');
+
+function resolvePassive(user, enemy, context, trigger) {
   let logs = [];
   const champName = user.name;
   if (passives[champName] && typeof passives[champName].passive === 'function') {
-    const log = passives[champName].passive(user, enemy, context, event);
+    const log = passives[champName].passive(user, enemy, context, trigger);
     if (log) logs.push(log);
   }
   return logs;
-};
+}
+
+module.exports = resolvePassive;
