86ae1f597ed870cb441612c772c610b8b581493b
Mysian <lym3123@naver.com>
2025-06-18 14:41:11 +0900

    Update pass.js

--- godbot/battle-system/pass.js
+++ godbot/battle-system/pass.js
@@ -2,7 +2,7 @@ const { getChampionNameByUserId } = require('../utils/champion-utils');
 
 module.exports = function pass(user, enemy, context, logs) {
   logs = logs || [];
-  logs.push(`😴 ${getChampionNameByUserId(user.id)}가 휴식을 취함!`);
+  logs.push(`😴 ${getChampionNameByUserId(user.id)}가 우물에서 쉽니다!`);
   // 어떤 상태든 무조건 휴식만 기록!
   return logs;
 };
