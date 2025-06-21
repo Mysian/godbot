7206f091870381c33fac4f058db70027f9cc3dbb
Mysian <lym3123@naver.com>
2025-06-21 11:47:14 +0900

    Add files via upload

---
+++ godbot/commands/kr-friend.js
@@ -0,0 +1,34 @@
+// commands/friend.js
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const relationship = require('../utils/relationship.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('우정')
+    .setDescription('특정 유저를 생각하는 자신의 관계 확인')
+    .addUserOption(opt =>
+      opt.setName('유저').setDescription('대상 유저').setRequired(true)
+    ),
+  async execute(interaction) {
+    const me = interaction.user.id;
+    const target = interaction.options.getUser('유저').id;
+
+    if (me === target) {
+      return interaction.reply({ content: '자기 자신과의 관계는 항상 "무관심"입니다.', ephemeral: true });
+    }
+
+    const score = relationship.getScore(me, target);
+    const rel = relationship.getRelation(me, target);
+
+    const embed = new EmbedBuilder()
+      .setTitle('나의 관계도')
+      .addFields(
+        { name: '대상', value: `<@${target}>`, inline: true },
+        { name: '관계', value: rel, inline: true },
+        { name: '호감도 점수', value: String(score), inline: true }
+      )
+      .setColor(0x43e743);
+
+    return interaction.reply({ embeds: [embed], ephemeral: true });
+  }
+};
---
+++ godbot/commands/kr-relation.js
@@ -0,0 +1,38 @@
+// commands/relation.js
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const relationship = require('../utils/relationship.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('관계')
+    .setDescription('두 유저간의 관계 확인')
+    .addUserOption(opt =>
+      opt.setName('유저1').setDescription('첫번째 유저').setRequired(true)
+    )
+    .addUserOption(opt =>
+      opt.setName('유저2').setDescription('두번째 유저').setRequired(true)
+    ),
+  async execute(interaction) {
+    const user1 = interaction.options.getUser('유저1').id;
+    const user2 = interaction.options.getUser('유저2').id;
+
+    if (user1 === user2) {
+      return interaction.reply({ content: '동일인 간의 관계는 항상 "무관심"입니다.', ephemeral: true });
+    }
+
+    const score1 = relationship.getScore(user1, user2);
+    const rel1 = relationship.getRelation(user1, user2);
+    const score2 = relationship.getScore(user2, user1);
+    const rel2 = relationship.getRelation(user2, user1);
+
+    const embed = new EmbedBuilder()
+      .setTitle('유저 간 관계도')
+      .addFields(
+        { name: `<@${user1}> → <@${user2}>`, value: `관계: ${rel1}\n점수: ${score1}`, inline: true },
+        { name: `<@${user2}> → <@${user1}>`, value: `관계: ${rel2}\n점수: ${score2}`, inline: true }
+      )
+      .setColor(0x00bfff);
+
+    return interaction.reply({ embeds: [embed], ephemeral: true });
+  }
+};
