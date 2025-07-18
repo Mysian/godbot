7847da99b06d5c93ca317b57bc88a157f6943ad0
Mysian <lym3123@naver.com>
2025-07-18 12:26:25 +0900

    Create pulseok.js

---
+++ godbot/commands/pulseok.js
@@ -0,0 +1,16 @@
+// commands/pulseok.js
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('풀석')
+    .setDescription('풀썩 쓰러지는 RP!'),
+  async execute(interaction) {
+    const nickname = interaction.member?.nickname || interaction.user.username;
+    await interaction.reply({
+      content: `헉! '${nickname}' 님이 풀썩 쓰러지고 말았습니다!`,
+      ephemeral: false
+    });
+  }
+};
