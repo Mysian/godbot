810c9b2e738ebca7db5064c6c6785e2642e2ad17
Mysian <lym3123@naver.com>
2025-09-12 13:54:24 +0900

    Update util.js

--- godbot/commands/util.js
+++ godbot/commands/util.js
@@ -372,7 +372,7 @@ module.exports = {
     .setDescription("유틸리티 도구 모음")
     .addSubcommand(sc => sc.setName("계산기").setDescription("버튼 계산기"))
     .addSubcommand(sc => sc.setName("메모장").setDescription("개인 메모/검색/수정/삭제"))
-    .addSubcommand(sc => sc.setName("복권번호").setDescription("1~45 중 6개, 총 5줄")),
+    .addSubcommand(sc => sc.setName("복권번호").setDescription("1~45 중 6개, 총 5줄"))
     .addSubcommand(sc => sc.setName("마법의소라고동").setDescription("봇이 그래/아니 답변"))
 
   
