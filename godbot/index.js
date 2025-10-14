68afaab7769b1d3a6f8b585d6d6c6924e59f2cf1
Mysian <lym3123@naver.com>
2025-10-15 00:27:59 +0900

    Update index.js

--- godbot/index.js
+++ godbot/index.js
@@ -101,9 +101,6 @@ controlPanel.register(client);
 const { startSecretChannels } = require('./utils/secret-channels.js');
 startSecretChannels(client);
 
-const { createLotterySystem } = require('./utils/lottery');
-createLotterySystem(client, '1427667597901566024');
-
 client.on(Events.GuildCreate, async guild => {
   const logChannel = await client.channels.fetch(LOG_CHANNEL_ID).catch(() => null);
   const ts = new Date().toLocaleString("ko-KR", { timeZone: "Asia/Seoul" });
